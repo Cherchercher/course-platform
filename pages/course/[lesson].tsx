0f29c99d7d3cf94be56f7b77f5823ef00af99ac7
YassinEldeeb <yassineldeeb94@gmail.com>
2021-11-01 01:13:20 +0200

    fix utility fetcher function

--- pages/course/[lesson].tsx
+++ pages/course/[lesson].tsx
@@ -21,8 +21,55 @@ const components = {
   Head,
 };
 
+<<<<<<< HEAD
 export default function LessonPage({ source, frontMatter }: Props) {
   const { data: session, status } = useSession();
+=======
+const LoadingSkeleton = () => {
+  return (
+    <div className="flex justify-center max-w-screen-md mx-auto my-24">
+      <ContentLoader
+        speed={2}
+        width={'100%'}
+        height={'100%'}
+        viewBox="0 0 557 305"
+        backgroundColor="#f5f5f5"
+        foregroundColor="#efefef"
+      >
+        <rect x="0" y="0" rx="3" ry="3" width="370" height="30" />
+        <rect x="0" y="190" rx="4" ry="4" width="72" height="15" />
+        <rect x="0" y="90" rx="4" ry="4" width="557" height="15" />
+        <rect x="0" y="115" rx="4" ry="4" width="305" height="15" />
+        <rect x="0" y="140" rx="4" ry="4" width="557" height="15" />
+        <rect x="0" y="165" rx="4" ry="4" width="557" height="15" />
+        <rect x="0" y="215" rx="4" ry="4" width="418" height="15" />
+        <rect x="0" y="240" rx="4" ry="4" width="557" height="15" />
+        <rect x="0" y="265" rx="4" ry="4" width="533" height="15" />
+        <rect x="0" y="290" rx="4" ry="4" width="344" height="15" />
+        <rect x="0" y="43" rx="3" ry="3" width="431" height="21" />
+      </ContentLoader>
+    </div>
+  );
+};
+const ErrorComponent = () => {
+  return (
+    <div className="prose max-w-screen-md mx-auto my-24">
+      <h1> Error: Couldn&apos;t find this Lesson! </h1>
+      <p>
+        Maybe the Lesson was removed by the author, please contact support :D
+      </p>
+    </div>
+  );
+};
+export default function LessonPage() {
+  const router = useRouter();
+
+  const { lesson } = router.query;
+  const { data, error } = useSWR<LessonResponse>(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/api/lessons/${lesson}`,
+    fetcher,
+  );
+>>>>>>> e5c2bb7 (fix utility fetcher function)
 
   return (
     <>
